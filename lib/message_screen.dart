889e6fc6a89082fece54b8ac950a8d5897de4555
usernamerokyzzz <emailmuhammad.535230104@stu.untar.ac.id>
2024-09-26 10:18:58 +0700

    Two commit

--- lib/message_screen.dart
+++ lib/message_screen.dart
@@ -68,6 +68,17 @@ class MessageScreen extends StatelessWidget {
             ),
           ),
           SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              "Active Now",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
           SizedBox(
             height: 90,
             child: ListView.builder(
@@ -105,12 +116,83 @@ class MessageScreen extends StatelessWidget {
                           ),
                         ),
                       ),
+                      Container(
+                        margin: EdgeInsets.all(4),
+                        padding: EdgeInsets.all(3),
+                        height: 20,
+                        width: 20,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                      )
                     ],
                   ),
                 );
               },
             ),
           ),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              "Recent Chat",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          ListView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: 6,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: ListTile(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ChatScreen(),
+                          ));
+                    },
+                    leading: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage(
+                        "images/${imgs[index]}",
+                      ),
+                    ),
+                    title: Text(
+                      "Dr. Doctor Name",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      "Hello, Doctor Are you there?",
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black54,
+                      ),
+                    ),
+                    trailing: Text(
+                      "12:30",
+                      style: TextStyle(fontSize: 15, color: Colors.black54),
+                    ),
+                  ),
+                );
+              })
         ],
       ),
     );
